d8bad1aebe0e61eccb9fbd0a0754feb129c560af
Marek Štec <mar.stec@seznam.cz>
2024-09-05 11:56:11 +0200

    v1.1.4

--- constants.py
+++ constants.py
@@ -22,6 +22,31 @@ def get_syslog_server() -> str:
         return rows[0]
     except Exception as e:
         print(e)
+        return ""
+
+
+def get_swap_wie() -> bool:
+    """Returns a boolean to determine whether open and monitor pins are swaped with reader pins"""
+    try:
+        mydb = mysql.connector.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASS,
+            database=DB_NAME,
+        )
+        cur = mydb.cursor()
+        cur.execute(
+            "SELECT VALUE AS v FROM ConfigDU WHERE PROPERTY='swap_wiegand_pins'"
+        )
+        rows = cur.fetchone()
+        cur.close()
+        mydb.close()
+        if len(rows) != 1:
+            return False
+        return bool(int(rows[0]))
+    except Exception as e:
+        print(e)
+        return False
 
 
 def get_mac(interface: str = "eth0") -> str:
@@ -34,6 +59,12 @@ def get_mac(interface: str = "eth0") -> str:
     return mac
 
 
+# DATABASE
+DB_HOST = "localhost"
+DB_USER = "ma"
+DB_PASS = "FrameWork5414*"
+DB_NAME = "MeritAccessLocal"
+
 # PINOUT
 SYS_LED_RED = 0
 SYS_LED_GREEN = 1
@@ -42,22 +73,24 @@ CONFIG_BTN = 3
 R1_RED_LED = 23
 R1_GREEN_LED = 24
 R1_BEEP = 25
-OPEN1 = 27
-MONITOR1 = 17
 RELAY1 = 18
 R2_RED_LED = 16
 R2_GREEN_LED = 20
 R2_BEEP = 21
-OPEN2 = 13
-MONITOR2 = 6
 RELAY2 = 12
 
 
-# DATABASE
-DB_HOST = "localhost"
-DB_USER = "ma"
-DB_PASS = "FrameWork5414*"
-DB_NAME = "MeritAccessLocal"
+if not get_swap_wie():
+    OPEN1 = 27
+    MONITOR1 = 17
+    OPEN2 = 13
+    MONITOR2 = 6
+else:
+    OPEN1 = 5
+    MONITOR1 = 22
+    OPEN2 = 26
+    MONITOR2 = 19
+
 
 # ACCESS POINT
 AP_PASS = "meritmerit"
